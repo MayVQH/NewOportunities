c1182afb12b556044633dc1f31ab714bc0ae782a
MayVQH <mayra.herrera@grupovanquish.com.mx>
2025-04-06 10:30:25 -0600

    Cambios para obtener foto de perfil

--- microsoft.js
+++ microsoft.js
@@ -1,6 +1,8 @@
 import passport from "passport"
 import { Strategy as MicrosoftStrategy } from "passport-microsoft";
 import {config} from "dotenv"
+import axios from "axios";
+
 
 config()
 
@@ -11,10 +13,32 @@ passport.use("auth-microsoft", new MicrosoftStrategy({
     scope: ["user.read","calendars.read","mail.read","offline_access"],
     authorizationURL:"https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
     tokenURL:'https://login.microsoftonline.com/common/oauth2/v2.0/token'
-}, function (accessToken,refreshToken,profile,done) {
+}, async function (accessToken,refreshToken,profile,done) {
+    try {
+        const photoResponse = await axios.get('https://graph.microsoft.com/v1.0/me/photo/$value',
+            {
+                headers:{
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                responseType: 'arraybuffer'
+            }
+        );
+
+        const photoBase64 = Buffer.from(photoResponse.data, 'binary').toString('base64');
+        const photoUrl = `data:${photoResponse.headers['content-type']};base64,${photoBase64}`;
+
+        profile.photos = [{value:photoUrl}];
+        console.log(profile)
+        done(null,profile)
+
+
+    } catch (error){
+        console.error('Error al obtener la imagen de perfil: ',error.message);
+        profile.photo = [];
+        done(null,profile);
+    }
     profile._json.photo = profile.photos && profile.photos[0] && profile.photos[0].value;
     console.log(profile);
 
     done(null,profile)
-}
-));
+}));
